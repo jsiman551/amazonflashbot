8769f67976858c0f629fedabf81080f61f2d3d5e
Jose Simancas <josesimancas51@gmail.com>
2025-02-11 16:50:17 -0500

    Feat: First Commit

---
+++ src/bot.ts
@@ -0,0 +1,22 @@
+import TelegramBot from 'node-telegram-bot-api';
+
+const token: string = '7820367617:AAEA2xdlYAfhb24qLkKhnvs33NHetUQhmbs';
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('new_chat_members', (msg) => {
+    if (!msg.new_chat_members || msg.new_chat_members.length === 0) {
+        return;
+    }
+    const newUser = msg.new_chat_members[0];
+    if (msg.chat.type === 'group' || msg.chat.type === 'supergroup') {
+        const welcomeMessage: string = `Welcome to @SuperDealsAmazonBot, ${newUser.first_name}! Stay tuned for amazing Amazon deals and flash sales. 🚀💸`;
+        bot.sendMessage(msg.chat.id, welcomeMessage);
+    }
+});
+
+// add commands, so bot can respond to them
+bot.onText(/\/start/, (msg) => {
+  const chatId: string = msg.chat.id.toString();
+  bot.sendMessage(chatId, 'Welcome to @SuperDealsAmazonBot! Here you will find the best Amazon deals. Stay tuned! 🚀💸');
+});
